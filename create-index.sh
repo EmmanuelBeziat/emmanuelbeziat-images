d0cbd8b3f892d71745a5a07e9837f236d8554e54
Emmanuel Béziat <social@emmanuelbeziat.com>
2024-10-20 23:57:45 +0200

    perf: :zap: Replace content index if needed

--- create-index.sh
+++ create-index.sh
@@ -4,15 +4,23 @@
 SOURCE="$(pwd)/pages/viewer.php"  # Absolute path to the viewer.php file
 TARGET_DIR="$(pwd)/media"          # Absolute path to the media directory
 
-# Find all directories in the target directory, excluding .well-known, and create an index.php file
+# Find all directories in the target directory, excluding .well-known, and create or update index.php
 find "$TARGET_DIR" -type d -not -path "$TARGET_DIR/.well-known/*" -exec sh -c '
-    for dir; do
-        INDEX_FILE="$dir/index.php"
-        if [ ! -e "$INDEX_FILE" ]; then
-            echo "Creating index.php in: $dir"
-            echo "<?php include \"$0\"; ?>" > "$INDEX_FILE"  # Create index.php that includes viewer.php
-        else
-            echo "index.php already exists in: $dir"
-        fi
-    done
+	for dir; do
+		INDEX_FILE="$dir/index.php"
+		NEW_CONTENT="<?php include \"$0\"; ?>"
+
+		if [ -e "$INDEX_FILE" ]; then
+			# Compare existing content with new content
+			if ! cmp -s <(echo "$NEW_CONTENT") "$INDEX_FILE"; then
+				echo "Updating index.php in: $dir"
+				echo "$NEW_CONTENT" > "$INDEX_FILE"
+			else
+				echo "index.php is already up to date in: $dir"
+			fi
+		else
+			echo "Creating index.php in: $dir"
+			echo "$NEW_CONTENT" > "$INDEX_FILE"
+		fi
+	done
 ' "$SOURCE" {} +
