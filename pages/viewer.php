1e08da7f3c41f49323447d29550d35a67fa21285
Emmanuel Béziat <social@emmanuelbeziat.com>
2024-10-21 02:42:03 +0200

    fix: :bug: Test path

--- pages/viewer.php
+++ pages/viewer.php
@@ -29,7 +29,7 @@ $files = array_map(fn($fileInfo) => (object) [
 
 asort($files);
 
-$public_path = dirname($_SERVER['SCRIPT_NAME'])
+$public_path = $public_path = __DIR__;
 ?>
 <!doctype html>
 <html lang="fr">
